55016048c708a249951e7b2540d98d3a37d2f0e0
Romisaa Fadel <163692583+romisaa5@users.noreply.github.com>
2025-03-03 01:10:39 +0200

    finish class transaction & bankaccount

--- week 5/bank system/ATM.dart
+++ week 5/bank system/ATM.dart
@@ -3,7 +3,7 @@ import 'bankacount.dart';
 import 'transaction.dart';
 
 class ATM {
-  void performTransaction(Transaction transaction, BankAccount account) {
-    transaction.execute(account);
+  void performTransaction(Transaction transaction, BankAccount account,String language) {
+    transaction.execute(account,language);
   }
 }
--- week 5/bank system/bankacount.dart
+++ week 5/bank system/bankacount.dart
@@ -1,31 +1,32 @@
 import 'transaction.dart';
 
 class BankAccount {
-  int acountNumber;
-  String acountName;
-  String acountPassword;
-  Transaction transactionHistory;
+  int accountNumber;
+  String accountName;
+  String accountPassword;
+  List<Transaction> transactionHistory = [];
   double balance;
+
   BankAccount({
-    required this.acountName,
-    required this.acountNumber,
-    required this.acountPassword,
-    required this.transactionHistory,
+    required this.accountName,
+    required this.accountNumber,
+    required this.accountPassword,
     required this.balance,
   });
+
   void updateBalance(double amount) {
     balance += amount;
   }
 
   void checkBalance(String language) {
     if (language == "ar") {
-      print("\n💰 رصيدك الحالي: \$${balance}\n");
+      print("\n💰 رصيدك الحالي: \$${balance.toStringAsFixed(2)}\n");
     } else {
-      print("\n💰 Your current balance: \$${balance}\n");
+      print("\n💰 Your current balance: \$${balance.toStringAsFixed(2)}\n");
     }
   }
 
   bool verifyPin(String enteredPin) {
-    return enteredPin == acountPassword;
+    return enteredPin == accountPassword;
   }
 }
