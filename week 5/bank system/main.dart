c00423adee591aa1efaa4322888f483e29188e33
Romisaa Fadel <163692583+romisaa5@users.noreply.github.com>
2025-03-06 02:48:03 +0200

    update task week 5

--- week 5/bank system/main.dart
+++ week 5/bank system/main.dart
@@ -12,27 +12,33 @@ void main() {
   String choice = stdin.readLineSync()!;
   String language = (choice == "1") ? "ar" : "en";
 
- BankAccount userAccount = BankAccount(
+  BankAccount userAccount = BankAccount(
       accountName: 'Romisaa Fadel',
       accountNumber: 101,
       accountPassword: '15917674R',
       balance: 10800);
-   ATM atm = ATM(language: language);
-   while (true) {
+  ATM atm = ATM(language: language);
+  while (true) {
     print(language == "ar"
         ? "\n🔹 اختر العملية: \n 1️⃣  سحب  \n 2️⃣  إيداع  \n 3️⃣  استعلام عن الرصيد \n 4️⃣   خروج"
         : "\n🔹 Select operation: \n  1️⃣  Withdraw \n 2️⃣  Deposit \n  3️⃣  Check balance \n 4️⃣  Exit");
-    stdout.write(language == "ar" ? "👉 أدخل رقم العملية: " : "👉 Enter operation number: ");
+    stdout.write(language == "ar"
+        ? "👉 أدخل رقم العملية: "
+        : "👉 Enter operation number: ");
     String choice = stdin.readLineSync()!;
 
     switch (choice) {
       case "1":
-        stdout.write(language == "ar" ? "💵 أدخل المبلغ للسحب: " : "💵 Enter amount to withdraw: ");
+        stdout.write(language == "ar"
+            ? "💵 أدخل المبلغ للسحب: "
+            : "💵 Enter amount to withdraw: ");
         double amount = double.parse(stdin.readLineSync()!);
         atm.performTransaction(WithdrawTransaction(amount), userAccount);
         break;
       case "2":
-        stdout.write(language == "ar" ? "💰 أدخل المبلغ للإيداع: " : "💰 Enter amount to deposit: ");
+        stdout.write(language == "ar"
+            ? "💰 أدخل المبلغ للإيداع: "
+            : "💰 Enter amount to deposit: ");
         double amount = double.parse(stdin.readLineSync()!);
         atm.performTransaction(DepositTransaction(amount), userAccount);
         break;
@@ -40,11 +46,12 @@ void main() {
         atm.performTransaction(CheckBalanceTransaction(), userAccount);
         break;
       case "4":
-        print(language == "ar" ? "👋 شكراً لاستخدامك الـ ATM!" : " Thank you for using the ATM! 👋");
+        print(language == "ar"
+            ? "👋 شكراً لاستخدامك الـ ATM!"
+            : " Thank you for using the ATM! 👋");
         return;
       default:
         print(language == "ar" ? "❌ خيار غير صالح!" : " Invalid option! ❌");
     }
   }
-
 }
