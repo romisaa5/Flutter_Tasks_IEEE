0a30db959a6481e7bfe1b25553821b72d0890650
Romisaa Fadel <163692583+romisaa5@users.noreply.github.com>
2025-03-28 21:32:21 +0200

    Edit main function

--- final project begginer/systems/Hotel_Booking_System/main.dart
+++ final project begginer/systems/Hotel_Booking_System/main.dart
@@ -1,33 +1,38 @@
 import 'hotel.dart';
 import 'room.dart';
 import 'user.dart';
+import 'admin.dart';
+import 'payment.dart';
+import 'review.dart';
 
 void main() {
   Hotel hotel = Hotel("Grand Hotel");
 
-  Room room1 = Room(101, 100);
-  Room room2 = Room(102, 150);
-  Room room3 = Room(103, 200);
-
-  hotel.addRoom(room1);
-  hotel.addRoom(room2);
-  hotel.addRoom(room3);
+  Admin admin = Admin("Manager", hotel);
+  admin.addRoom(Room(101, 100));
+  admin.addRoom(Room(102, 150));
+  admin.addRoom(Room(103, 200));
 
   hotel.showAvailableRooms();
 
-  User user1 = User("Ali", "U001"); 
+  User user1 = User("Ali", "U001");
 
-  user1.makeBooking(room1); 
-  user1.makeBooking(room1); 
+ 
+  user1.makeBooking(hotel.rooms[0]);
 
-  hotel.showAvailableRooms();
-
-
+ 
   if (user1.bookings.isNotEmpty) {
-    user1.cancelBooking(user1.bookings.first.room.roomNumber);
-  } else {
-    print("No bookings to cancel.");
+    Payment payment = Payment(user: user1, booking: user1.bookings.first);
+    payment.makePayment();
   }
 
+
+  Review review = Review(user: user1, comment: "Great hotel with amazing service!", rating: 5);
+  review.showReview();
+
+ 
+  admin.showAllRooms();
+
+  admin.removeRoom(102);
   hotel.showAvailableRooms();
 }
