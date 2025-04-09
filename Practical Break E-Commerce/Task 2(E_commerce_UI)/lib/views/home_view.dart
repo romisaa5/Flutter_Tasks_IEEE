e1296e6d93a9b9481d55a61dc92563af3b9906c0
Romisaa Fadel <163692583+romisaa5@users.noreply.github.com>
2025-04-09 04:12:41 +0200

    Add  home view

--- Practical Break E-Commerce/Task 2(E_commerce_UI)/lib/views/home_view.dart
+++ Practical Break E-Commerce/Task 2(E_commerce_UI)/lib/views/home_view.dart
@@ -1,10 +1,42 @@
+
 import 'package:flutter/material.dart';
+import 'package:task2/widgets/custom_bannar.dart';
+import 'package:task2/widgets/custom_build_header.dart';
+import 'package:task2/widgets/custom_list_new_items.dart';
+import 'package:task2/widgets/custom_list_sale_items.dart';
+
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Column(
+        spacing: 10,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+         CustomBannar(),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0,),
+          child: Column(
+            spacing: 10,
+            children: [
+              CustomBuildHeader(
+                ontap: (){},
+                title: 'Sale', 
+                subTitle: 'Super summer sale'),
+                CustomListSaleItems(),
+                 CustomBuildHeader(
+                ontap: (){},
+                title: 'New', 
+                subTitle: 'You’ve never seen it before!'),
+              CustomListNewItems()
+            ],
+          ),
+        ),
+        ],
+      ),
+    );
   }
 }
