6c6bfecb9c26bd42b733e2c583fca53241a37c5b
Romisaa Fadel <163692583+romisaa5@users.noreply.github.com>
2025-04-10 05:36:29 +0200

    Edit Task 2.1 at week 9

--- week 9/Task 2.1/lib/home_screen.dart
+++ week 9/Task 2.1/lib/home_screen.dart
@@ -5,6 +5,34 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Gallary', style: TextStyle(color: Colors.grey)),
+        centerTitle: true,
+      ),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 5,
+          crossAxisSpacing: 5,
+          childAspectRatio: 0.7,
+        ),
+        itemCount: 20,
+        physics: const BouncingScrollPhysics(),
+
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            margin: const EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                image: AssetImage('assets/images/img${(index % 3) + 1}.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
