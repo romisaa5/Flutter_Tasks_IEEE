4060d64460e72cb589854b7ce93928a87b5dba09
Romisaa Fadel <163692583+romisaa5@users.noreply.github.com>
2025-04-29 17:56:45 +0200

    Edit

--- Practical Break (2)/task1/lib/constants.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Kprimarycolor = Colors.white;
-const Ksecondrycolor = Colors.grey;
--- Practical Break E-Commerce/Task 2(E_commerce_UI)/lib/core/constants.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/cupertino.dart';
- const KprimaryColor = Color(0xffDB3022);
-const KsecondryColor = Color(0xff000000);
-const KbackgroundColor = Color(0xffF9F9F9);
