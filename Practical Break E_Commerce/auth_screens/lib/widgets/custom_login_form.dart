b40c75cf0adbf7b094c027e3d780dbb4fcf6bbf4
Romisaa Fadel <163692583+romisaa5@users.noreply.github.com>
2025-07-02 00:40:35 +0200

    Task 1 done

--- Practical Break E_Commerce/auth_screens/lib/widgets/custom_login_form.dart
+++ Practical Break E_Commerce/auth_screens/lib/widgets/custom_login_form.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:auth_screens/helper/validation_methods.dart';
 import 'package:auth_screens/theme/app_color.dart';
 import 'package:auth_screens/theme/text_styles.dart';
@@ -10,14 +12,24 @@ import 'package:auth_screens/widgets/custom_text_form_field.dart';
 import 'package:auth_screens/widgets/remember_me.dart';
 import 'package:flutter/material.dart';
 
-class CustomLoginForm extends StatelessWidget {
+class CustomLoginForm extends StatefulWidget {
   const CustomLoginForm({super.key, required this.hight});
   final double hight;
+
+  @override
+  State<CustomLoginForm> createState() => _CustomLoginFormState();
+}
+
+class _CustomLoginFormState extends State<CustomLoginForm> {
+  bool isshowPassword = false;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width,
-      height: hight,
+      height: widget.hight,
       decoration: BoxDecoration(
         color: AppColor.whiteColor,
         borderRadius: BorderRadius.only(
@@ -27,81 +39,106 @@ class CustomLoginForm extends StatelessWidget {
       ),
       child: SingleChildScrollView(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          spacing: 10,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Email', style: Styles.textStyle18),
-            AppTextFormField(
-              hintText: 'Enter your email',
-              validator: (value) => ValidationMethods.validateEmail(value),
-            ),
-            SizedBox(height: 10),
-            Text('Password', style: Styles.textStyle18),
-            AppTextFormField(
-              hintText: 'Enter your password',
-              validator: (value) => ValidationMethods.validatePassword(value),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                RememberMe(),
-                TextButton(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            spacing: 10,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Email', style: Styles.textStyle18),
+              AppTextFormField(
+                controller: emailController,
+                hintText: 'Enter your email',
+                validator: (value) => ValidationMethods.validateEmail(value),
+              ),
+              SizedBox(height: 10),
+              Text('Password', style: Styles.textStyle18),
+              AppTextFormField(
+                onChanged: (value) {},
+                maxLines: 1,
+                isObscureText: !isshowPassword,
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    isshowPassword ? Icons.visibility : Icons.visibility_off,
+                    color: AppColor.greydark,
+                  ),
                   onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return ForgetPassword();
-                        },
-                      ),
-                    );
+                    setState(() {
+                      isshowPassword = !isshowPassword;
+                    });
                   },
-                  child: Text(
-                    'Forgot Password?',
-                    style: Styles.textStyle12.copyWith(
-                      color: AppColor.greydarker,
+                ),
+                controller: passwordController,
+                hintText: 'Enter your password',
+                validator: (value) => ValidationMethods.validatePassword(value),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  RememberMe(),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return ForgetPassword();
+                          },
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'Forgot Password?',
+                      style: Styles.textStyle12.copyWith(
+                        color: AppColor.greydarker,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            CustomButton(
-              text: 'Login',
-              color: AppColor.kPrimaryColor,
-              width: double.infinity,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Don’t have an account yet? Register',
-                  style: Styles.textStyle12,
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return RegisterView();
-                        },
+                ],
+              ),
+              CustomButton(
+                onTap: () {
+                  if (_formKey.currentState!.validate()) {
+                    log('Email: ${emailController.text}');
+                    log('Password: ${passwordController.text}');
+                  }
+                },
+                text: 'Login',
+                color: AppColor.kPrimaryColor,
+                width: double.infinity,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Don’t have an account yet? Register',
+                    style: Styles.textStyle12,
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return RegisterView();
+                          },
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'here',
+                      style: Styles.textStyle12.copyWith(
+                        color: AppColor.kPrimaryColor,
                       ),
-                    );
-                  },
-                  child: Text(
-                    'here',
-                    style: Styles.textStyle12.copyWith(
-                      color: AppColor.kPrimaryColor,
                     ),
                   ),
-                ),
-              ],
-            ),
-            CustomDivider(text: 'Or login with'),
-            SizedBox(height: 5),
-            CustomFacebookGmail(),
-          ],
+                ],
+              ),
+              CustomDivider(text: 'Or login with'),
+              SizedBox(height: 5),
+              CustomFacebookGmail(),
+            ],
+          ),
         ),
       ),
     );
--- Practical Break E_Commerce/auth_screens/lib/widgets/custom_register_form.dart
+++ Practical Break E_Commerce/auth_screens/lib/widgets/custom_register_form.dart
@@ -8,15 +8,30 @@ import 'package:auth_screens/widgets/custom_facebook_gmail.dart';
 import 'package:auth_screens/widgets/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 
-class CustomRegisterForm extends StatelessWidget {
+class CustomRegisterForm extends StatefulWidget {
   const CustomRegisterForm({super.key, required this.hight});
 
   final double hight;
+
+  @override
+  State<CustomRegisterForm> createState() => _CustomRegisterFormState();
+}
+
+class _CustomRegisterFormState extends State<CustomRegisterForm> {
+  bool isshowPassword = false;
+  bool isshowConfirmPassword = false;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width,
-      height: hight,
+      height: widget.hight,
       decoration: BoxDecoration(
         color: AppColor.whiteColor,
         borderRadius: BorderRadius.only(
@@ -26,75 +41,121 @@ class CustomRegisterForm extends StatelessWidget {
       ),
       child: SingleChildScrollView(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          spacing: 10,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 20),
-            Text('Name', style: Styles.textStyle18),
-            AppTextFormField(
-              hintText: 'Your Name, e.g : John Doe',
-              validator: (value) => ValidationMethods.validateUsername(value),
-            ),
-            Text('Email', style: Styles.textStyle18),
-            AppTextFormField(
-              hintText: 'Enter your email',
-              validator: (value) => ValidationMethods.validateEmail(value),
-            ),
-            Text('Phone Number', style: Styles.textStyle18),
-            AppTextFormField(
-              hintText: 'Your phone number, e.g : +01 112 xxx xxx',
-              validator: (value) => ValidationMethods.validatephone(value),
-            ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            spacing: 10,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),
+              Text('Name', style: Styles.textStyle18),
+              AppTextFormField(
+                controller: nameController,
+                onChanged: (value) {},
+                hintText: 'Your Name, e.g : John Doe',
+                validator: (value) => ValidationMethods.validateUsername(value),
+              ),
+              Text('Email', style: Styles.textStyle18),
+              AppTextFormField(
+                controller: emailController,
+                hintText: 'Enter your email',
+                validator: (value) => ValidationMethods.validateEmail(value),
+              ),
+              Text('Phone Number', style: Styles.textStyle18),
+              AppTextFormField(
+                controller: phoneController,
+                hintText: 'Your phone number, e.g : +01 112 xxx xxx',
+                validator: (value) => ValidationMethods.validatephone(value),
+              ),
 
-            Text('Password', style: Styles.textStyle18),
-            AppTextFormField(
-              hintText: 'Enter your password',
-              validator: (value) => ValidationMethods.validatePassword(value),
-            ),
-            Text('Confirm Password', style: Styles.textStyle18),
-            AppTextFormField(
-              hintText: 'Re-type your password',
-              validator:
-                  (value) => ValidationMethods.validateConfirmPassword(value),
-            ),
-
-            CustomButton(
-              text: 'Register',
-              color: AppColor.kPrimaryColor,
-              width: double.infinity,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'already have an account? Login',
-                  style: Styles.textStyle12,
-                ),
-                TextButton(
+              Text('Password', style: Styles.textStyle18),
+              AppTextFormField(
+                controller: passwordController,
+                onChanged: (value) {},
+                maxLines: 1,
+                isObscureText: !isshowPassword,
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    isshowPassword ? Icons.visibility : Icons.visibility_off,
+                    color: AppColor.greydark,
+                  ),
                   onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return LoginView();
-                        },
-                      ),
-                    );
+                    setState(() {
+                      isshowPassword = !isshowPassword;
+                    });
                   },
-                  child: Text(
-                    'here',
-                    style: Styles.textStyle12.copyWith(
-                      color: AppColor.kPrimaryColor,
+                ),
+                hintText: 'Enter your password',
+                validator: (value) => ValidationMethods.validatePassword(value),
+              ),
+              Text('Confirm Password', style: Styles.textStyle18),
+              AppTextFormField(
+                controller: confirmPasswordController,
+                onChanged: (value) {},
+                maxLines: 1,
+                isObscureText: !isshowConfirmPassword,
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    isshowConfirmPassword
+                        ? Icons.visibility
+                        : Icons.visibility_off,
+                    color: AppColor.greydark,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      isshowConfirmPassword = !isshowConfirmPassword;
+                    });
+                  },
+                ),
+                hintText: 'Re-type your password',
+                validator:
+                    (value) => ValidationMethods.validateConfirmPassword(value),
+              ),
+
+              CustomButton(
+                onTap: () {
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Registration Successful')),
+                    );
+                  }
+                },
+                text: 'Register',
+                color: AppColor.kPrimaryColor,
+                width: double.infinity,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'already have an account? Login',
+                    style: Styles.textStyle12,
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return LoginView();
+                          },
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'here',
+                      style: Styles.textStyle12.copyWith(
+                        color: AppColor.kPrimaryColor,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            CustomDivider(text: 'Or register with'),
-            SizedBox(height: 5),
-            CustomFacebookGmail(),
-          ],
+                ],
+              ),
+              CustomDivider(text: 'Or register with'),
+              SizedBox(height: 5),
+              CustomFacebookGmail(),
+            ],
+          ),
         ),
       ),
     );
