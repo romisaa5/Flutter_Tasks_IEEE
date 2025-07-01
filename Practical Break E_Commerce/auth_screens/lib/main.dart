c11176fc60f81852face26ac22859c08be34b16c
Romisaa Fadel <163692583+romisaa5@users.noreply.github.com>
2025-07-01 16:54:20 +0200

    start Week Practical Break: E-Commerce

---
+++ Practical Break E_Commerce/auth_screens/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AuthScreens());
+}
+
+class AuthScreens extends StatelessWidget {
+  const AuthScreens({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    
+    );
+  }
+}
+
---
+++ Practical Break E_Commerce/auth_screens/lib/theme/app_color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class AppColor {
+  static const Color kPrimaryColor = Color(0xffF9A826);
+  static const Color blackColor = Color(0xff000000);
+  static const Color whiteColor = Color(0xffFFFFFF);
+  static const Color greyLightest = Color(0xFFEEEEEE);
+  static const Color greyLight = Color(0xFFADADAD);
+  static const Color greydark = Color(0xffEFF1F3);
+  static const Color greyDarkest = Color(0xff606060);
+}
